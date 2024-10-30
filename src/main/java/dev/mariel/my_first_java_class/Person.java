68a3c57ae1bfd2f881ace1d7527b5ad487bdb8ab
Codebreakerblack <blancohmariel@gmail.com>
2024-10-30 10:36:42 +0100

    add new attributes to the class

--- src/main/java/dev/mariel/my_first_java_class/Person.java
+++ src/main/java/dev/mariel/my_first_java_class/Person.java
@@ -5,13 +5,17 @@ public class Person {
     private String lastName;
     private String idNumber;
     private int birthYear;
+    private String countryOfBirth;
+    private char gender;
 
 
-    public Person(String firstName, String lastName, String idNumber, int birthYear) {
+    public Person(String firstName, String lastName, String idNumber, int birthYear, String countryOfBirth, char gender) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.idNumber = idNumber;
         this.birthYear = birthYear;
+        this.countryOfBirth = countryOfBirth;
+        this.gender = gender;
     }
 
     public void print() {
@@ -19,6 +23,9 @@ public class Person {
         System.out.println("Last Name: " + lastName);
         System.out.println("ID Number: " + idNumber);
         System.out.println("Birth Year: " + birthYear);
+        System.out.println("Country of Birth: " + countryOfBirth);
+        System.out.println("Gender: " + gender);
+        
     }
 
 }
